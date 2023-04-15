94d34ad6bb3e09c9b1e9bde6b54193c095d66803
Marc Donald <dev@marcdonald.com>
2023-04-16 00:15:39 +0100

    feat: add site

---
+++ apps/site/components/WipBanner.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils';
+
+const WipBanner = ({ className }: { className?: string }) => {
+	return (
+		<div
+			className={cn(
+				'border-b border-b-slate-200 bg-gray-100 p-2 text-center text-slate-900 dark:border-b-slate-600 dark:bg-slate-700 dark:text-slate-50',
+				className
+			)}
+		>
+			🚧 This project is a work in progress 🚧
+		</div>
+	);
+};
+
+export default WipBanner;
